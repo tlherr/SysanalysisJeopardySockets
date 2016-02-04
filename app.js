3a44a2b5a1cccd8b618500ddf627dc38544f51f4
Thomas Herr <thom1810@gmail.com>
2016-02-04 15:14:12 -0500

    Working out a few bugs

--- app.js
+++ app.js
@@ -1,14 +1,32 @@
 var express = require('express');
-var cookieParser = require('cookie-parser')
+var cookieParser = require('cookie-parser');
 var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var parsedJSON = require('./jeopardy.json');
+
+
+var counter = 1;
 var people = {};
+var admin = null;
 var activeAvailable = false;
 var currentQuestion = null;
 var currentAnswerer = null;
+var password = guid();
 
-app.use(cookieParser())
+console.log("The Admin Password is: "+password);
+
+function guid() {
+    function s4() {
+        return Math.floor((1 + Math.random()) * 0x10000)
+            .toString(16)
+            .substring(1);
+    }
+    return s4() + s4() + '-' + s4() + '-' + s4() + '-' +
+        s4() + '-' + s4() + s4() + s4();
+}
+
+app.use(cookieParser());
 app.use(express.static('public'));
 
 app.get('/', function(req, res){
@@ -17,8 +35,10 @@ app.get('/', function(req, res){
 
 //document.cookie="password=thesecretpassword"
 app.get('/admin', function(req, res) {
-   if(req.cookies.password=="thesecretpassword") {
+   if(req.cookies.password==password) {
        res.sendFile(__dirname + "/admin.html");
+   } else {
+       res.sendStatus(401);
    }
 });
 
@@ -26,24 +46,68 @@ io.on("connection", function (socket) {
 
     console.log("A New Client Connected");
 
-    socket.on("join", function(name){
+    socket.on("joined", function(){
+        var name = "Student-"+counter;
+        counter++;
         console.log("New Client Joined: "+ name);
         people[socket.id] = name;
+        socket.emit("received_name", name);
         socket.emit("update", "You have connected to the server.");
         io.sockets.emit("update-people", people);
     });
 
-    socket.on("buzzing" , function(name) {
-       console.log("Buzzing from: "+name);
+    socket.on("admin_joined", function() {
+        admin = socket.id;
+        people[socket.id] = "Admin";
+    });
 
-        if(activeAvailable && currentAnswerer==null && currentQuestion) {
+    socket.on("get_question_data", function() {
+        if(socket.id==admin) {
+            socket.emit("question_data_received", parsedJSON);
+        }
+    });
+
+    socket.on("set_questions_open", function() {
+        if(socket.id==admin) {
+            activeAvailable = true;
+        }
+    });
+
+    socket.on("reset_answerer", function() {
+       if(socket.id==admin) {
+           currentAnswerer = null;
+       }
+    });
+
+    socket.on("question_answered", function(results, answerer, question, col, row) {
+        if(socket.id==admin) {
+            io.sockets.emit("client_question_answered", answerer, results[1].value, question, col, row);
+            if(results[1].value) {
+                io.sockets[Object.keys(people)[currentAnswerer].id].emit("modify_points", results[1].value, question);
+            }
+            activeAvailable = true;
+        }
+    });
+
+    socket.on("buzzing" , function(col, question) {
+       console.log("Buzzing from: "+people[socket.id]);
+
+        if(activeAvailable && currentAnswerer==null) {
             //A question is available to be answered and no one is answering it, allow this person to answer
             currentAnswerer = people[socket.id];
-            socket.emit("buzz_success", currentQuestion);
-            io.sockets.emit("answerer_selected", name, currentQuestion);
+            socket.emit("buzz_success");
+            io.sockets.emit("answerer_selected", people[socket.id], col, question);
         } else {
             socket.emit("buzz_failure");
         }
+    });
+
+    socket.on("name_change", function(newName) {
+        var oldName = people[socket.id];
+        people[socket.id] = newName;
+        console.log(oldName+" has been renamed to "+newName);
+        io.sockets.emit("update", oldName+" has been renamed to "+newName);
+        io.sockets.emit("update-people", people);
     });
 
 
--- public/js/main.js
+++ public/js/main.js
@@ -1,71 +1,103 @@
 (function($) {
+    $('document').ready(function() {
+        var socket = io();
+        var currentAnswerer = null;
+        var currentQuestion = null;
+        var currentCol = null;
+        var currentRow = null;
+        socket.emit("admin_joined");
+        socket.emit("get_question_data");
 
-    var socket = io();
-    var currentQuestion = null;
-
-    $('#questionanswerholder').on('show.bs.modal', function (e) {
-        var modal = $(this);
-        var a = $(e.relatedTarget);
-
-        currentQuestion = a;
-
-        var answerText = modal.find("p.answer");
-        answerText.text(a.data('jeopardy-answer'));
-
-        var questionText = modal.find("p.question");
-        questionText.text(a.data('jeopardy-question'));
-
-
-        var pointValue = modal.find("input#point-value");
-        pointValue.val(a.data('jeopardy-value'));
-
-        var date = new Date();
-        date.setSeconds(date.getSeconds() + 20);
-        $('#clock').countdown(date, function(event) {
-            console.log(event.type);
-            $(this).html(event.strftime('%S'));
-
-            if(event.type==="finish") {
-                new Audio('/audio/time.mp3').play()
+        socket.on("question_data_received", function(data) {
+            for (var i = 0; i < data.questions.length; i++) {
+                var questions = data.questions[i].vals;
+                for (var x = 0; x < questions.length; x++) {
+                    var element = $('#question-'+i+'-'+x+ ' > a');
+                    element.data('jeopardy-value', data.questions[x].vals[i].points);
+                    element.data('jeopardy-question', data.questions[x].vals[i].question);
+                    element.data('jeopardy-answer', data.questions[x].vals[i].answer);
+                    element.text(data.questions[x].vals[i].points);
+                }
             }
         });
 
-    }).on('hide.bs.modal', function(e) {
-        var modal = $(this);
-        var pointValue = modal.find("input#point-value");
-        pointValue.val(0);
-        $(".question-container").removeClass("animate flipInY").addClass("hidden");
-        $(".answer-container").removeClass("animated flipOutY");
+        socket.on("answerer_selected", function(name, col, question) {
+           //Show the name in the label
+            $('span.current_answerer').text(name);
+            $('#answerer').val(name);
+            currentAnswerer = name;
+            currentCol = col;
+            currentRow = question;
+            $('#question-'+col+'-'+question+ ' > a').trigger('click');
+        });
+
+        $('#open-question-button').click(function(e) {
+            e.preventDefault();
+            $(this).addClass('btn-danger').removeClass('btn-successs');
+            socket.emit("set_questions_open");
+        });
+
+        $('#reset-answerer-button').click(function(e) {
+            e.preventDefault();
+            socket.emit("reset_answerer");
+        });
+
+        $('#questionanswerholder').on('show.bs.modal', function (e) {
+            var modal = $(this);
+            var a = $(e.relatedTarget);
+
+            currentQuestion = a;
+
+            var answerText = modal.find("p.answer");
+            answerText.text(a.data('jeopardy-answer'));
+
+            var questionText = modal.find("p.question");
+            questionText.text(a.data('jeopardy-question'));
+
+            var pointValue = modal.find("input#point-value");
+            pointValue.val(a.data('jeopardy-value'));
+
+            var date = new Date();
+            date.setSeconds(date.getSeconds() + 20);
+            $('#clock').countdown(date, function(event) {
+                console.log(event.type);
+                $(this).html(event.strftime('%S'));
+
+                if(event.type==="finish") {
+                    new Audio('/audio/time.mp3').play()
+                }
+            });
+
+        }).on('hide.bs.modal', function(e) {
+            var modal = $(this);
+            var pointValue = modal.find("input#point-value");
+            pointValue.val(0);
+            $(".question-container").removeClass("animate flipInY").addClass("hidden");
+            $(".answer-container").removeClass("animated flipOutY");
+        });
+
+        $('.answer-container').click(function(e){
+            $(this).addClass("animated flipOutY");
+            $(".question-container").removeClass("hidden").addClass("animated flipInY");
+        });
+
+        $("#doScore").submit(function( event ) {
+            event.preventDefault();
+
+             var results = $(this).serializeArray();
+
+            socket.emit("question_answered", results, currentAnswerer, currentQuestion.data(), currentCol, currentRow);
+            $('span.current_answerer').text("");
+            currentAnswerer = null;
+            currentCol = null;
+            currentRow = null;
+            $('#answerer').val("");
+
+            $('#questionanswerholder').modal("hide");
+            $(currentQuestion).parent().addClass("disabled");
+            currentQuestion = null;
+        });
+
+
     });
-
-
-    $('.answer-container').click(function(e){
-        $(this).addClass("animated flipOutY");
-        $(".question-container").removeClass("hidden").addClass("animated flipInY");
-    });
-
-    $("#doScore").submit(function( event ) {
-        event.preventDefault();
-
-        var results = $(this).serializeArray();
-
-        var correct = parseInt(results[1].value);
-        var span = $('#team-score-'+results[0].value).find('span');
-        var currentScore = parseInt(span.text());
-
-        if(correct==1) {
-            currentScore+=parseInt(results[2].value);
-        } else if(correct==0) {
-            currentScore-=parseInt(results[2].value);
-        }
-
-        span.text(currentScore);
-
-
-        $('#questionanswerholder').modal("hide");
-        $(currentQuestion).parent().addClass("disabled");
-        currentQuestion = null;
-    });
-
-
 })(jQuery);
